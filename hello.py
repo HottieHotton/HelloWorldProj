7d20439006213aa1f47bc38ec0dd540c45b3f0ce
Braxton Hotton <bhotton25@gmail.com>
2025-03-07 21:14:45 -0700

    Added code

---
+++ hello.py
@@ -0,0 +1,8 @@
+# Hello World Program
+import webbrowser, time
+
+print("Hello World")
+print("I hope you're having a great day!")
+time.sleep(1)
+
+webbrowser.open("https://www.youtube.com/watch?v=kqmfWMvyRDE")
